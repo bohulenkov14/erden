c1ac5b44407274c98abbed4468c8e7aede255f63
darvit <andrey.d.matveev@gmail.com>
2017-10-14 18:38:18 +0300

    Add EventSourcing configuration

---
+++ src/Erden.EventSourcing/Core/AutoRegistrator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.Loader;
+using Microsoft.DotNet.InternalAbstractions;
+using Microsoft.Extensions.DependencyModel;
+
+namespace Erden.EventSourcing.Core
+{
+    /// <summary>
+    /// Auto registration handlers in event bus
+    /// </summary>
+    internal sealed class AutoRegistrator
+    {
+        private IServiceProvider provider;
+
+        public AutoRegistrator(IServiceProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        public void Register(Type handlerType, Type registrator)
+        {
+            var runtimeId = RuntimeEnvironment.GetRuntimeIdentifier();
+            var assemblies = DependencyContext.Default.GetRuntimeAssemblyNames(runtimeId);
+            var loadContext = AssemblyLoadContext.Default;
+
+            List<TypeInfo> types = new List<TypeInfo>();
+            foreach (var assemblyName in assemblies)
+            {
+                var assembly = loadContext.LoadFromAssemblyName(assemblyName);
+                types.AddRange(assembly.DefinedTypes
+                    .Where(p => p.GetInterfaces()
+                    .Where(i => i.GetTypeInfo().IsGenericType
+                        && i.GetGenericTypeDefinition() == handlerType).Count() > 0));
+            }
+
+            var registerMethod = registrator.GetMethod("Register");
+
+            foreach (var type in types)
+            {
+                var interfaces = type.GetInterfaces();
+                foreach (var @interface in interfaces)
+                {
+                    Type[] typeArguments = @interface.GenericTypeArguments;
+                    MethodInfo handleMethod = type.GetMethod("Handle", new[] { typeArguments[0] });
+                    MethodInfo genericRegister = registerMethod.MakeGenericMethod(typeArguments);
+
+                    var parameter = Expression.Parameter(typeArguments[0]);
+                    var instance = Expression.Constant(provider.GetService(type.AsType()));
+                    var handleMethodCall = Expression.Call(instance, handleMethod, parameter);
+                    var lambda = Expression.Lambda(handleMethodCall, parameter).Compile();
+
+                    genericRegister.Invoke(provider.GetService(registrator), new object[] { lambda });
+                }
+            }
+        }
+    }
+}
---
+++ src/Erden.EventSourcing/ESConfiguration.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+using Erden.EventSourcing.Core;
+
+namespace Erden.EventSourcing
+{
+    public sealed class ESConfiguration
+    {
+        /// <summary>
+        /// DI container
+        /// </summary>
+        private readonly IServiceCollection services;
+        /// <summary>
+        /// Assemblies, where command and query handlers placed
+        /// </summary>
+        private readonly List<Assembly> assemblies = new List<Assembly>();
+
+        public ESConfiguration(IServiceCollection services)
+        {
+            this.services = services;
+        }
+
+        /// <summary>
+        /// Build configuration
+        /// </summary>
+        public void Build()
+        {
+            foreach (var assembly in assemblies)
+            {
+                RegisterHandlers(assembly);
+            }
+
+            var registrator = new AutoRegistrator(services.BuildServiceProvider());
+            registrator.Register(typeof(IEventHandler<>), typeof(IEventHandlerRegistrator));
+        }
+
+        public ESConfiguration UseDefaultEventBus()
+        {
+            var inMemoryEventBus = new InMemoryEventBus();
+            services.AddSingleton<IEventHandlerRegistrator>(provider => inMemoryEventBus);
+            services.AddSingleton<IEventPublisher>(provider => inMemoryEventBus);
+            return this;
+        }
+
+        public ESConfiguration WithAssembly(Assembly assembly)
+        {
+            assemblies.Add(assembly);
+            return this;
+        }
+
+        /// <summary>
+        /// Register command and query handlers
+        /// </summary>
+        /// <param name="assembly">Assembly for registration</param>
+        private void RegisterHandlers(Assembly assembly)
+        {
+            services.Scan(scan => scan
+                .FromAssemblies(assembly)
+                    .AddClasses(classes => classes.Where(x => {
+                        var allInterfaces = x.GetInterfaces();
+                        return
+                            allInterfaces.Any(y => 
+                                y.GetTypeInfo().IsGenericType
+                                && y.GetTypeInfo().GetGenericTypeDefinition() == typeof(IEventHandler<>));
+                    }))
+                    .AsSelf()
+                    .WithTransientLifetime()
+            );
+        }
+    }
+}
