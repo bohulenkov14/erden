b3218404ba10561a271cf528c4f761b582ca2825
darvit <andrey.d.matveev@gmail.com>
2017-10-14 18:20:26 +0300

    Add some CQRS tests

---
+++ tests/Erden.Cqrs.Tests/InMemoryCommandBusTest.cs
@@ -0,0 +1,118 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+using Xunit;
+
+namespace Erden.Cqrs.Tests
+{
+    public sealed class InMemoryCommandBusTest
+    {
+        [Fact]
+        public async Task Command_Should_Be_Executed_Test()
+        {
+            var provider = Configure();
+
+            ICommandBus bus = provider.GetService<ICommandBus>();
+
+            var entity = new TestEntity { TestProperty = 0 };
+            await bus.Send(new FirstTestCommand(entity));
+            Assert.Equal(1, entity.TestProperty);
+        }
+
+        [Fact]
+        public async Task Command_Should_Be_Executed_Multiple_Time_Test()
+        {
+            var provider = Configure();
+
+            ICommandBus bus = provider.GetService<ICommandBus>();
+
+            var entity = new TestEntity { TestProperty = -1 };
+
+            for (int i = 0; i < 3; ++i)
+            {
+                await bus.Send(new FirstTestCommand(entity));
+                Assert.Equal(i, entity.TestProperty);
+            }
+        }
+
+        [Fact]
+        public async Task Different_Commands_Should_Be_Executed_Test()
+        {
+            var provider = Configure();
+
+            ICommandBus bus = provider.GetService<ICommandBus>();
+
+            var entity = new TestEntity { TestProperty = 0 };
+            await bus.Send(new FirstTestCommand(entity));
+            Assert.Equal(1, entity.TestProperty);
+            await bus.Send(new SecondTestCommand(entity));
+            Assert.Equal(2, entity.TestProperty);
+        }
+
+        private ServiceProvider Configure()
+        {
+            var services = new ServiceCollection();
+            var config = new CqrsConfiguration(services)
+                .UseDefaultCommandBus()
+                .UseDefaultDataStorage()
+                .WithAssembly(typeof(InMemoryCommandBusTest).GetTypeInfo().Assembly);
+            config.Build();
+
+            return services.BuildServiceProvider();
+        }
+    }
+
+    internal sealed class TestEntity
+    {
+        public int TestProperty { get; set; }
+    }
+
+    internal sealed class FirstTestCommand : BaseCommand
+    {
+        public FirstTestCommand(TestEntity entity)
+        {
+            Entity = entity;
+        }
+
+        public TestEntity Entity { get; private set; }
+
+        public override Task Log()
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    internal sealed class SecondTestCommand : BaseCommand
+    {
+        public SecondTestCommand(TestEntity entity)
+        {
+            Entity = entity;
+        }
+
+        public TestEntity Entity { get; private set; }
+
+        public override Task Log()
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    internal sealed class FirstTestCommandHandler : ICommandHandler<FirstTestCommand>
+    {
+        public Task Execute(FirstTestCommand command)
+        {
+            command.Entity.TestProperty++;
+            return Task.CompletedTask;
+        }
+    }
+
+    internal sealed class SecondTestCommandHandler : ICommandHandler<SecondTestCommand>
+    {
+        public Task Execute(SecondTestCommand command)
+        {
+            command.Entity.TestProperty++;
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ tests/Erden.Cqrs.Tests/InMemoryDataStorageTest.cs
@@ -0,0 +1,96 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+using Xunit;
+
+namespace Erden.Cqrs.Tests
+{
+    public sealed class InMemoryDataStorageTest
+    {
+        public const string LOREM = "Lorem";
+        public const string IPSUM = "Ipsum";
+
+        [Fact]
+        public async Task Query_Should_Be_Executed_Test()
+        {
+            var provider = Configure();
+
+            IDataStorage storage = provider.GetService<IDataStorage>();
+
+            var result = await storage.Retrieve(new FirstTestQuery());
+            Assert.Equal("Lorem", result);
+        }
+
+        [Fact]
+        public async Task Query_Should_Be_Executed_Multiple_Time_Test()
+        {
+            var provider = Configure();
+
+            IDataStorage storage = provider.GetService<IDataStorage>();
+
+            for (int i = 0; i < 3; ++i)
+            {
+                var result = await storage.Retrieve(new FirstTestQuery());
+                Assert.Equal(LOREM, result);
+            }
+        }
+
+        [Fact]
+        public async Task Different_Queries_Should_Be_Executed_Test()
+        {
+            var provider = Configure();
+
+            IDataStorage storage = provider.GetService<IDataStorage>();
+
+            var firstResult = await storage.Retrieve(new FirstTestQuery());
+            Assert.Equal(LOREM, firstResult);
+            var secondResult = await storage.Retrieve(new SecondTestQuery());
+            Assert.Equal(IPSUM, secondResult);
+        }
+
+        private ServiceProvider Configure()
+        {
+            var services = new ServiceCollection();
+            var config = new CqrsConfiguration(services)
+                .UseDefaultCommandBus()
+                .UseDefaultDataStorage()
+                .WithAssembly(typeof(InMemoryCommandBusTest).GetTypeInfo().Assembly);
+            config.Build();
+
+            return services.BuildServiceProvider();
+        }
+    }
+
+    internal sealed class FirstTestQuery : BaseQuery<string>
+    {
+        public override Task Log()
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    internal sealed class SecondTestQuery : BaseQuery<string>
+    {
+        public override Task Log()
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    internal sealed class FirstTestQueryHandler : IQueryHandler<FirstTestQuery, string>
+    {
+        public Task<string> Execute(FirstTestQuery query)
+        {
+            return Task.Run(() => InMemoryDataStorageTest.LOREM);
+        }
+    }
+
+    internal sealed class SecondTestQueryHandler : IQueryHandler<SecondTestQuery, string>
+    {
+        public Task<string> Execute(SecondTestQuery query)
+        {
+            return Task.Run(() => InMemoryDataStorageTest.IPSUM);
+        }
+    }
+}
