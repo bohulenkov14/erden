a68e555834e7c8bc9a8891a862f5ff8366163137
darvit <andrey.d.matveev@gmail.com>
2017-10-15 23:53:53 +0300

    Add DomainCommand

---
+++ src/Erden.Cqrs/DomainCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Erden.Cqrs
+{
+    public abstract class DomainCommand : BaseCommand
+    {
+        public DomainCommand(Guid entityId)
+        {
+            EntityId = entityId;
+        }
+
+        public Guid EntityId { get; private set; }
+    }
+}
