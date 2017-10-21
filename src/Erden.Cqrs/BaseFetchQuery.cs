e14a7ed50b9ba6cc285493222e9ce7a620a80b4a
darvit <andrey.d.matveev@gmail.com>
2017-10-22 00:44:22 +0300

    Add base fetch query (with paging)

---
+++ src/Erden.Cqrs/BaseFetchQuery.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace Erden.Cqrs
+{
+    public abstract class BaseFetchQuery<TResult> : BaseQuery<TResult> where TResult : class
+    {
+        public BaseFetchQuery(int offset, int size)
+        {
+            Offset = offset;
+            Size = size;
+        }
+
+        [JsonProperty("offset")]
+        public int Offset { get; private set; }
+        [JsonProperty("size")]
+        public int Size { get; private set; }
+    }
+}
