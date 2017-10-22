a878e73710c3fb7d149cd1d0400802fe40442810
darvit <andrey.d.matveev@gmail.com>
2017-10-22 20:04:52 +0300

    Update ErdenConfig, remove duplication of Core functions

--- src/Erden.Configuration/ErdenConfig.cs
+++ src/Erden.Configuration/ErdenConfig.cs
@@ -1,10 +1,6 @@
-﻿using System.Linq;
-using System.Reflection;
-using System.Runtime.Loader;
-using Microsoft.DotNet.InternalAbstractions;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyModel;
+﻿using Microsoft.Extensions.DependencyInjection;
 
+using Erden.Core;
 using Erden.Cqrs;
 using Erden.Dal;
 using Erden.Domain;
@@ -26,17 +22,12 @@ namespace Erden.Configuration
             services.AddSingleton<IAggregateStorage, AggregateStorage>();
             services.AddSingleton<ISession, Session>();
 
-            var runtimeId = RuntimeEnvironment.GetRuntimeIdentifier();
-            var assemblies = DependencyContext.Default.GetRuntimeAssemblyNames(runtimeId);
-            var loadContext = AssemblyLoadContext.Default;
-
-            foreach (var assemblyName in assemblies)
-            {
-                var assembly = loadContext.LoadFromAssemblyName(assemblyName);
-                RegisterHandlers(assembly);
-            }
-
-            var registrator = new AutoRegistrator(services.BuildServiceProvider());
+            var registrator = new AutoRegistrator(services);
+            registrator.AddHandlers(typeof(IEventHandler<>));
+            registrator.AddHandlers(typeof(ICommandHandler<>));
+            registrator.AddHandlers(typeof(IQueryHandler<,>));
+            registrator.AddHandlers(typeof(IChangeHandler<>));
+            registrator.AddHandlers(typeof(IFetchHandler<,>));
             registrator.Register(typeof(IEventHandler<>), typeof(IEventHandlerRegistrator), "Handle");
             registrator.Register(typeof(ICommandHandler<>), typeof(ICommandHandlerRegistrator), "Execute");
             registrator.Register(typeof(IQueryHandler<,>), typeof(IQueryHandlerRegistrator), "Execute");
@@ -95,38 +86,6 @@ namespace Erden.Configuration
             services.AddSingleton<IChangesBus>(provider => inMemoryChangesBus);
             services.AddSingleton<IChangeHandlerRegistrator>(provider => inMemoryChangesBus);
             return this;
-        }
-
-        /// <summary>
-        /// Register command and query handlers
-        /// </summary>
-        /// <param name="assembly">Assembly for registration</param>
-        private void RegisterHandlers(Assembly assembly)
-        {
-            services.Scan(scan => scan
-                .FromAssemblies(assembly)
-                    .AddClasses(classes => classes.Where(x => {
-                        var allInterfaces = x.GetInterfaces();
-                        return
-                            allInterfaces.Any(y =>
-                                y.GetTypeInfo().IsGenericType
-                                && y.GetTypeInfo().GetGenericTypeDefinition() == typeof(ICommandHandler<>)) ||
-                            allInterfaces.Any(y =>
-                                y.GetTypeInfo().IsGenericType
-                                && y.GetTypeInfo().GetGenericTypeDefinition() == typeof(IQueryHandler<,>)) ||
-                            allInterfaces.Any(y =>
-                                y.GetTypeInfo().IsGenericType
-                                && y.GetTypeInfo().GetGenericTypeDefinition() == typeof(IChangeHandler<>)) ||
-                            allInterfaces.Any(y =>
-                                y.GetTypeInfo().IsGenericType
-                                && y.GetTypeInfo().GetGenericTypeDefinition() == typeof(IFetchHandler<,>)) ||
-                            allInterfaces.Any(y =>
-                                y.GetTypeInfo().IsGenericType
-                                && y.GetTypeInfo().GetGenericTypeDefinition() == typeof(IEventHandler<>));
-                    }))
-                    .AsSelf()
-                    .WithTransientLifetime()
-            );
         }
     }
 }
