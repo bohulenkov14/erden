b1605b0ca48b5734c3cdde57008e8e74162a8e73
darvit <andrey.d.matveev@gmail.com>
2017-10-30 00:06:04 +0300

    Add method to ISession to retrieve AggregateRoot of any version

--- src/Erden.Domain/ISession.cs
+++ src/Erden.Domain/ISession.cs
@@ -3,10 +3,36 @@ using System.Threading.Tasks;
 
 namespace Erden.Domain
 {
+    /// <summary>
+    /// Current session, where stores used aggregate roots
+    /// </summary>
     public interface ISession
     {
+        /// <summary>
+        /// Add aggregate to traking list
+        /// </summary>
+        /// <typeparam name="T">Aggregate type</typeparam>
+        /// <param name="aggregate">Aggregate instance</param>
         Task Add<T>(T aggregate) where T : AggregateRoot;
+        /// <summary>
+        /// Get aggregate
+        /// </summary>
+        /// <typeparam name="T">Aggregate type</typeparam>
+        /// <param name="id">Aggregate ID</param>
+        /// <param name="version">Aggregate version</param>
+        /// <returns>Aggregate instance</returns>
         Task<T> Get<T>(Guid id, long version) where T : AggregateRoot;
+        /// <summary>
+        /// Get aggregate
+        /// </summary>
+        /// <typeparam name="T">Aggregate type</typeparam>
+        /// <param name="id">Aggregate ID</param>
+        /// <returns>Aggregate instance</returns>
+        Task<T> Get<T>(Guid id) where T : AggregateRoot;
+        /// <summary>
+        /// Commit changes for aggregate with specified ID
+        /// </summary>
+        /// <param name="id">Aggregate ID</param>
         Task Commit(Guid id);
     }
 }
--- src/Erden.Domain/Session.cs
+++ src/Erden.Domain/Session.cs
@@ -6,23 +6,45 @@ using Erden.Domain.Exceptions;
 
 namespace Erden.Domain
 {
+    /// <summary>
+    /// Current session, where stores used aggregate roots
+    /// </summary>
     public class Session : ISession
     {
+        /// <summary>
+        /// Persistent aggregate storage
+        /// </summary>
         private readonly IAggregateStorage storage;
+        /// <summary>
+        /// Traking aggregates
+        /// </summary>
         private static Dictionary<Guid, AggregateRoot> trackingAggregates
             = new Dictionary<Guid, AggregateRoot>();
 
+        /// <summary>
+        /// Initialize a new instance of <see cref="Session"/> class
+        /// </summary>
+        /// <param name="storage">Persistent aggregate storage</param>
         public Session(IAggregateStorage storage)
         {
             this.storage = storage;
         }
 
+        /// <summary>
+        /// Add aggregate to traking list
+        /// </summary>
+        /// <typeparam name="T">Aggregate type</typeparam>
+        /// <param name="aggregate">Aggregate instance</param>
         public Task Add<T>(T aggregate) where T : AggregateRoot
         {
             trackingAggregates.Add(aggregate.Id, aggregate);
             return Task.CompletedTask;
         }
 
+        /// <summary>
+        /// Commit changes for aggregate with specified ID
+        /// </summary>
+        /// <param name="id">Aggregate ID</param>
         public async Task Commit(Guid id)
         {
             try
@@ -36,6 +58,13 @@ namespace Erden.Domain
             }
         }
 
+        /// <summary>
+        /// Get aggregate
+        /// </summary>
+        /// <typeparam name="T">Aggregate type</typeparam>
+        /// <param name="id">Aggregate ID</param>
+        /// <param name="version">Aggregate version</param>
+        /// <returns>Aggregate instance</returns>
         public async Task<T> Get<T>(Guid id, long version) where T : AggregateRoot
         {
             if (trackingAggregates.TryGetValue(id, out var aggregate))
@@ -48,6 +77,16 @@ namespace Erden.Domain
             aggregate = await storage.Get<T>(id, version);
             trackingAggregates.Add(id, aggregate);
             return aggregate as T;
+        }
+        /// <summary>
+        /// Get aggregate
+        /// </summary>
+        /// <typeparam name="T">Aggregate type</typeparam>
+        /// <param name="id">Aggregate ID</param>
+        /// <returns>Aggregate instance</returns>
+        public Task<T> Get<T>(Guid id) where T : AggregateRoot
+        {
+            return Get<T>(id, -2);
         }
     }
 }
