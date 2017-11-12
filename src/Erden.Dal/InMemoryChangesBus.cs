42712b9f27fdb12f95e5fad077cd780a7e6114bb
darvit <andrey.d.matveev@gmail.com>
2017-11-12 22:16:34 +0300

    Using exceptions id DAL

--- src/Erden.Dal/InMemoryChangesBus.cs
+++ src/Erden.Dal/InMemoryChangesBus.cs
@@ -2,13 +2,26 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
+using Erden.Dal.Exceptions;
+
 namespace Erden.Dal
 {
+    /// <summary>
+    /// Default realization of <see cref="IChangesBus"/> and <see cref="IChangeHandlerRegistrator"/>
+    /// </summary>
     public sealed class InMemoryChangesBus : IChangesBus, IChangeHandlerRegistrator
     {
+        /// <summary>
+        /// Handlers
+        /// </summary>
         private readonly Dictionary<Type, Func<IChangeRequest, Task>> handlers
             = new Dictionary<Type, Func<IChangeRequest, Task>>();
 
+        /// <summary>
+        /// Register handler
+        /// </summary>
+        /// <typeparam name="T">Change request type</typeparam>
+        /// <param name="handler">Handler</param>
         public void Register<T>(Func<T, Task> handler) where T : IChangeRequest
         {
             if (handler == null)
@@ -16,18 +29,34 @@ namespace Erden.Dal
 
             handlers.Add(typeof(T), x => handler((T)x));
         }
-
+        /// <summary>
+        /// Send request for execution
+        /// </summary>
+        /// <typeparam name="T">Change request type</typeparam>
+        /// <param name="request">Change request</param>
         public async Task Send<T>(T request) where T : IChangeRequest
         {
             if (request == null)
                 throw new ArgumentNullException("request");
 
             if (!handlers.TryGetValue(typeof(T), out var handler))
-                throw new Exception();
+                throw new ChangeHandlerNotFoundException(typeof(T));
 
             await handler.Invoke(request);
         }
-
+        /// <summary>
+        /// Send request for execution
+        /// </summary>
+        /// <typeparam name="T">Change request type</typeparam>
+        public Task Send<T>() where T : IChangeRequest
+        {
+            return Send((T)Activator.CreateInstance(typeof(T)));
+        }
+        /// <summary>
+        /// Send request for execution
+        /// </summary>
+        /// <typeparam name="T">Change request type</typeparam>
+        /// <param name="args">Args to create change request</param>
         public Task Send<T>(params object[] args) where T : IChangeRequest
         {
             return Send((T)Activator.CreateInstance(typeof(T), args));
--- src/Erden.Dal/InMemoryStorage.cs
+++ src/Erden.Dal/InMemoryStorage.cs
@@ -2,13 +2,27 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
+using Erden.Dal.Exceptions;
+
 namespace Erden.Dal
 {
+    /// <summary>
+    /// Default realization of <see cref="IStorage"/> and <see cref="IFetchHandlerRegistrator"/>
+    /// </summary>
     public sealed class InMemoryStorage : IStorage, IFetchHandlerRegistrator
     {
+        /// <summary>
+        /// Handlers
+        /// </summary>
         private readonly Dictionary<Type, Delegate> handlers
             = new Dictionary<Type, Delegate>();
 
+        /// <summary>
+        /// Register handler
+        /// </summary>
+        /// <typeparam name="T">Fetch request type</typeparam>
+        /// <typeparam name="TResult">Result type</typeparam>
+        /// <param name="handler">Handler</param>
         public void Register<T, TResult>(Func<T, Task<TResult>> handler)
             where T : IFetchRequest<TResult>
             where TResult : class
@@ -18,6 +32,12 @@ namespace Erden.Dal
             handlers.Add(typeof(T), handler);
         }
 
+        /// <summary>
+        /// Retrieve data
+        /// </summary>
+        /// <typeparam name="TResult">Data type</typeparam>
+        /// <param name="request">Fetch request</param>
+        /// <returns>Result</returns>
         public Task<TResult> Retrieve<TResult>(IFetchRequest<TResult> request)
             where TResult : class
         {
@@ -26,9 +46,27 @@ namespace Erden.Dal
                 return handler.DynamicInvoke(request) as Task<TResult>;
             }
 
-            throw new Exception();
+            throw new FetchHandlerNotFoundException(request.GetType());
         }
-
+        /// <summary>
+        /// Retrieve data
+        /// </summary>
+        /// <typeparam name="T">Fetch request type</typeparam>
+        /// <typeparam name="TResult">Data type</typeparam>
+        /// <returns>Result</returns>
+        public Task<TResult> Retrieve<T, TResult>()
+            where T : IFetchRequest<TResult>
+            where TResult : class
+        {
+            return Retrieve((T)Activator.CreateInstance(typeof(T)));
+        }
+        /// <summary>
+        /// Retrieve data
+        /// </summary>
+        /// <typeparam name="T">Fetch request type</typeparam>
+        /// <typeparam name="TResult">Data type</typeparam>
+        /// <param name="args">Args to create fetch request</param>
+        /// <returns>Result</returns>
         public Task<TResult> Retrieve<T, TResult>(params object[] args)
             where T : IFetchRequest<TResult>
             where TResult : class
