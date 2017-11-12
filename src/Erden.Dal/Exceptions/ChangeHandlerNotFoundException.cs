38d9b2f03e5970406d82f1a2924b6610866b6b16
darvit <andrey.d.matveev@gmail.com>
2017-11-12 22:14:52 +0300

    Add dal exceptions

---
+++ src/Erden.Dal/Exceptions/ChangeHandlerNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Erden.Dal.Exceptions
+{
+    /// <summary>
+    /// The exception that is thrown when change handler not found
+    /// </summary>
+    public class ChangeHandlerNotFoundException : Exception
+    {
+        /// <summary>
+        /// Initialize a new instance of the <see cref="ChangeHandlerNotFoundException"/> class with the type of handler which was not found
+        /// </summary>
+        /// <param name="handlerType">Handler type</param>
+        public ChangeHandlerNotFoundException(Type handlerType)
+            : base($"Change hanlder with type {handlerType.Name} not found")
+        { }
+    }
+}
---
+++ src/Erden.Dal/Exceptions/FetchHandlerNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Erden.Dal.Exceptions
+{
+    /// <summary>
+    /// The exception that is thrown when fetch handler not found
+    /// </summary>
+    public class FetchHandlerNotFoundException : Exception
+    {
+        /// <summary>
+        /// Initialize a new instance of the <see cref="FetchHandlerNotFoundException"/> class with the type of handler which was not found
+        /// </summary>
+        /// <param name="handlerType">Handler type</param>
+        public FetchHandlerNotFoundException(Type handlerType)
+            : base($"Fetch hanlder with type {handlerType.Name} not found")
+        { }
+    }
+}
